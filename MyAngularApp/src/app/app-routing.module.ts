1edf6321ee712517d7edbc6c88537c885266a03f
Ilan Piczenik <ilanpiczenik31@gmail.com>
2024-06-04 09:59:36 -0300

    prueba

--- MyAngularApp/src/app/app-routing.module.ts
+++ MyAngularApp/src/app/app-routing.module.ts
@@ -10,9 +10,10 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    component: HomePageComponent,
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MyAngularApp/src/app/app.module.ts
+++ MyAngularApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';  // Import FormsModule
+import { CommonModule } from '@angular/common';  // Import CommonModule
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +10,8 @@ import { AuthenticationModule } from './authentication/authentication.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, AuthenticationModule],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, AuthenticationModule ,FormsModule,  // Add FormsModule to imports array
+  CommonModule  ],
   providers: [],
   bootstrap: [AppComponent],
 })
