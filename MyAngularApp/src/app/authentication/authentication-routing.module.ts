937c23fabe136f8063b11ec99c0d914299be78a4
Agustin Garcia <AG292894@fi365.ort.edu.uy>
2024-06-04 09:29:01 -0300

    pagina redirige a authentication-page, luego se creo el boton que redirige a home-page

--- MyAngularApp/src/app/authentication/authentication-routing.module.ts
+++ MyAngularApp/src/app/authentication/authentication-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthenticationPageComponent } from './authentication-page/authentication-page.component';
+
+const routes: Routes = [{ path: '', component: AuthenticationPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
