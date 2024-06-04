26552d19c266f0f491214bcd7940c87d52e02c31
martinaRoll <marturoll11@gmail.com>
2024-06-04 09:54:22 -0300

    reflection

--- My web api/MyWebApi/Services/LoadAssembly/LoadAssemblyService.cs
+++ My web api/MyWebApi/Services/LoadAssembly/LoadAssemblyService.cs
@@ -7,14 +7,46 @@ namespace MyWebApi.Services.LoadAssembly
     public sealed class LoadAssemblyService<T> : ILoadAssemblyService<T>
         where T : class
     {
+        private const string ImportersPath = "./FileImporters";
+
         public List<T> GetAll(string directory)
         {
-            throw new NotImplementedException();
+            string[] filePaths = Directory.GetFiles(directory, "*.dll");
+            List<T> availableImporters = new();
+            
+            foreach (string file in filePaths)
+            {
+                if (FileIsDll(file)) 
+                {
+                    FileInfo dllFile  = new (file); 
+                    Assembly myAssembly = Assembly.LoadFile(dllFile.FullName); 
+
+                    foreach (Type type in myAssembly.GetTypes()) 
+                    {
+                        if (typeof(T).IsAssignableFrom(type) && !type.IsInterface)
+                        {
+                            T instance = (T)Activator.CreateInstance(type)!;
+                            availableImporters.Add(instance);
+                        }
+                    }
+                }
+            }
+            return availableImporters;
+        }
+        
+        private static bool FileIsDll(string file)
+        {
+            return file.EndsWith("dll");
         }
 
         public T GetByIdentifier(string identifier)
         {
-            throw new NotImplementedException();
+            string[] filePaths = Directory.GetFiles(ImportersPath, "*.dll");
+          
+            T? importer = GetAll(ImportersPath)
+                .FirstOrDefault(i => i.GetType().Equals(identifier));
+
+            return importer;
         }
     }
 }
